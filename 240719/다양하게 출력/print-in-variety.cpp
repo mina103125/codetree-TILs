a89430dd5cc5e4df0fdbccd1d88c81a29050e7f0
mina103125 <133721845+mina103125@users.noreply.github.com>
2024-07-19 14:04:28 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() 
+{
+    double pie=3.1415926535;
+    cout<<"Total days in Year"<<endl<<365<<endl<<"Circumference rate"<<endl;
+    cout << fixed << setprecision(10) << pie << endl;
+    return 0;
+}
